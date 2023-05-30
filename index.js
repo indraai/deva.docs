6f482e17b508aad9b94487101f9fab0f728f77f5
Quinn <quinn@indra.ai>
2023-05-30 12:05:02 -0700

    update docs deva with a raw method to avoid feecting parsing

--- index.js
+++ index.js
@@ -49,28 +49,20 @@ const DOCS = new Deva({
     which calls the correct document file then passes it to the feecting deva
     for parsing.
     ***************/
-    view(opts) {
-      return new Promise((resolve, reject) => {
-        const docArr = opts.q.text ? opts.q.text.split(':') : [];
-        const part = docArr[1] ? docArr[1].toUpperCase() : this.vars.part;
-        const fDoc = docArr.length ? docArr[0] + '.feecting' : 'main.feecting';
-        const fDocs = path.join(this.config.dir, 'docs');
-        const fPath = path.join(fDocs, fDoc);
-        try {
-          let doc = fs.readFileSync(fPath, 'utf8');
-          if (part) doc = doc.split(`::BEGIN:${part}`)[1].split(`::END:${part}`)[0];
-
-          this.question(`#feecting parse:${this.agent.key} ${doc}`).then(parsed => {
-            return resolve({
-              text: parsed.a.text,
-              html: parsed.a.html,
-              data: parsed.a.data,
-            });
-          });
-        } catch (err) {
-          return this.error(err, opts, reject);
-        }
-      });
+    doc(loc=false) {
+      const docArr = loc ? loc.split(':') : [];
+      const part = docArr[1] ? docArr[1].toUpperCase() : this.vars.part;
+      const fDoc = docArr.length ? docArr[0] + '.feecting' : 'main.feecting';
+      const fDocs = path.join(this.config.dir, 'docs');
+      const fPath = path.join(fDocs, fDoc);
+      try {
+        let doc = fs.readFileSync(fPath, 'utf8');
+        if (part) doc = doc.split(`::BEGIN:${part}`)[1].split(`::END:${part}`)[0];
+        return doc;
+      }
+      catch (err) {
+        return err;
+      }
     },
   },
   methods: {
@@ -81,7 +73,36 @@ const DOCS = new Deva({
     a document from the text parameter.
     ***************/
     view(packet) {
-      return this.func.view(packet);
+      const agent = this.agent();
+      return new Promise((resolve, reject) => {
+        const doc = this.func.doc(packet.q.text);
+        this.question(`#feecting parse:${agent.key} ${doc}`).then(feecting => {
+          return resolve({
+            text: feecting.a.text,
+            html: feecting.a.html,
+            data: feecting.a.data,
+          });
+        }).catch(err => {
+          return this.error(err, packet, reject);
+        })
+      });
+    },
+
+    /**************
+    method: raw
+    params: packet
+    describe: The raw method replays the request to the view function to return
+    a document from the text parameter.
+    ***************/
+    raw(packet) {
+      const agent = this.agent();
+      return new Promise((resolve, reject) => {
+        try {
+          const text = this.func.get(packet.q.text);
+          return resolve({text})
+        } catch (e) {
+          return this.error(e, packet, reject);
+        }
     },
 
     /**************
